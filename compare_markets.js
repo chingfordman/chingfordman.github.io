34d94fb8b72fcde55885c2ff03592976ca332811
Jon Urwin <j.urwin@herts.ac.uk>
2020-07-27 17:06:36 +0100

    update

--- compare_markets.js
+++ compare_markets.js
@@ -1,5 +1,5 @@
 var data=[];
-data.push({marketRunner:"Most Seats-Conservative",current_price:1.8,prev_price:1.8,new_date:"27-07-2020",old_date:"26-07-2020"});
+data.push({marketRunner:"Most Seats-Conservative",current_price:1.2,prev_price:1.8,new_date:"27-07-2020",old_date:"26-07-2020"});
 data.push({marketRunner:"Most Seats-Labour",current_price:2.26,prev_price:2.28,new_date:"27-07-2020",old_date:"26-07-2020"});
 data.push({marketRunner:"Most Seats-Liberal Democrat",current_price:120,prev_price:120,new_date:"27-07-2020",old_date:"26-07-2020"});
 data.push({marketRunner:"Trump To Leave Before End of 1st Term-No",current_price:1.14,prev_price:1.14,new_date:"27-07-2020",old_date:"26-07-2020"});
