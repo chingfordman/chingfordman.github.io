075de32d8549f293516d50cdc5851ab8c07da410
Jon Urwin <j.urwin@herts.ac.uk>
2020-08-14 18:34:54 +0100

    update

--- compare_markets_month.js
+++ compare_markets_month.js
@@ -1,1 +1,9 @@
 var data=[];
+data.push({marketRunner:"Most Seats-Conservative",current_price:1.81,prev_price:1.8,new_date:"14-08-2020",old_date:"15-07-2020"});
+data.push({marketRunner:"Most Seats-Labour",current_price:2.2,prev_price:2.28,new_date:"14-08-2020",old_date:"15-07-2020"});
+data.push({marketRunner:"Most Seats-Liberal Democrat",current_price:130,prev_price:120,new_date:"14-08-2020",old_date:"15-07-2020"});
+data.push({marketRunner:"Trump To Leave Before End of 1st Term-No",current_price:1.14,prev_price:1.12,new_date:"14-08-2020",old_date:"15-07-2020"});
+data.push({marketRunner:"Trump To Leave Before End of 1st Term-Yes",current_price:7.8,prev_price:8.2,new_date:"14-08-2020",old_date:"15-07-2020"});
+data.push({marketRunner:"Winning Party-Any Other",current_price:440,prev_price:220,new_date:"14-08-2020",old_date:"15-07-2020"});
+data.push({marketRunner:"Winning Party-Democrats",current_price:1.63,prev_price:1.58,new_date:"14-08-2020",old_date:"15-07-2020"});
+data.push({marketRunner:"Winning Party-Republicans",current_price:2.56,prev_price:2.72,new_date:"14-08-2020",old_date:"15-07-2020"});
