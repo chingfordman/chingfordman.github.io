68404d8a65ad6357d49fc21fb92899669cf4bf50
Jon Urwin <j.urwin@herts.ac.uk>
2020-08-19 19:06:46 +0100

    update

--- render.js
+++ render.js
@@ -5,16 +5,15 @@ function renderData(){
 //assumes it has access to the data[] array contained in the data.js file in the same folder 
 
 var strOutput="";
-
-
+strOutput+="<p class=\"text-light\">Current date : " + data[0].new_date +"</p>";
+strOutput+="<p class=\"text-light\">Prev date : " + data[0].old_date +"</p>";
 strOutput+="<table class=\"table table-striped table-dark table-hover\">";
 strOutput+= "<thead>";
     strOutput+="<tr>";
       strOutput+="<th scope=\"col\">Market-runner</th>";
       strOutput+="<th scope=\"col\">New price</th>";
       strOutput+="<th scope=\"col\">Old price</th>";
-      strOutput+="<th scope=\"col\">date2</th>";
-	  strOutput+="<th scope=\"col\">date1</th>";
+      
     strOutput+="</tr>";
   strOutput+="</thead>";
   strOutput+="<tbody>"
@@ -37,8 +36,8 @@ strOutput += "<tr>";
 	
 	//strOutput += "<td>"+data[i].current_price +"</td>";
 	strOutput += "<td>"+data[i].prev_price +"</td>";
-	strOutput += "<td>"+data[i].new_date +"</td>";
-	strOutput += "<td>"+data[i].old_date +"</td>";
+	//strOutput += "<td>"+data[i].new_date +"</td>";
+	//strOutput += "<td>"+data[i].old_date +"</td>";
 	strOutput+="</tr>";
 	
 							}
