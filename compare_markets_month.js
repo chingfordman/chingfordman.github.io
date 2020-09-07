7b8cda3ec30b9d83b3d3db52779ee9b1dd8173d4
Jon Urwin <j.urwin@herts.ac.uk>
2020-09-07 10:11:30 +0100

    update

--- compare_markets_month.js
+++ compare_markets_month.js
@@ -1,9 +1,9 @@
 var data=[];
-data.push({marketRunner:"Most Seats-Conservative",current_price:1.84,prev_price:1.78,new_date:"31-08-2020",old_date:"28-07-2020"});
-data.push({marketRunner:"Most Seats-Labour",current_price:2.18,prev_price:2.22,new_date:"31-08-2020",old_date:"28-07-2020"});
-data.push({marketRunner:"Most Seats-Liberal Democrat",current_price:140,prev_price:130,new_date:"31-08-2020",old_date:"28-07-2020"});
-data.push({marketRunner:"Trump To Leave Before End of 1st Term-No",current_price:1.06,prev_price:1.14,new_date:"31-08-2020",old_date:"28-07-2020"});
-data.push({marketRunner:"Trump To Leave Before End of 1st Term-Yes",current_price:15,prev_price:7.6,new_date:"31-08-2020",old_date:"28-07-2020"});
-data.push({marketRunner:"Winning Party-Any Other",current_price:1000,prev_price:270,new_date:"31-08-2020",old_date:"28-07-2020"});
-data.push({marketRunner:"Winning Party-Democrats",current_price:1.88,prev_price:1.54,new_date:"31-08-2020",old_date:"28-07-2020"});
-data.push({marketRunner:"Winning Party-Republicans",current_price:2.12,prev_price:2.82,new_date:"31-08-2020",old_date:"28-07-2020"});
+data.push({marketRunner:"Most Seats-Conservative",current_price:1.82,prev_price:1.81,new_date:"07-09-2020",old_date:"30-07-2020"});
+data.push({marketRunner:"Most Seats-Liberal Democrat",current_price:140,prev_price:120,new_date:"07-09-2020",old_date:"30-07-2020"});
+data.push({marketRunner:"Most Seats-Labour",current_price:2.2,prev_price:2.24,new_date:"07-09-2020",old_date:"30-07-2020"});
+data.push({marketRunner:"Trump To Leave Before End of 1st Term-No",current_price:1.06,prev_price:1.14,new_date:"07-09-2020",old_date:"30-07-2020"});
+data.push({marketRunner:"Trump To Leave Before End of 1st Term-Yes",current_price:12.5,prev_price:7.6,new_date:"07-09-2020",old_date:"30-07-2020"});
+data.push({marketRunner:"Winning Party-Any Other",current_price:1000,prev_price:400,new_date:"07-09-2020",old_date:"30-07-2020"});
+data.push({marketRunner:"Winning Party-Democrats",current_price:1.87,prev_price:1.55,new_date:"07-09-2020",old_date:"30-07-2020"});
+data.push({marketRunner:"Winning Party-Republicans",current_price:2.12,prev_price:2.78,new_date:"07-09-2020",old_date:"30-07-2020"});
--- compare_markets_week.js
+++ compare_markets_week.js
@@ -1,9 +1,9 @@
 var data=[];
-data.push({marketRunner:"Most Seats-Conservative",current_price:1.84,prev_price:1.82,new_date:"31-08-2020",old_date:"21-08-2020"});
-data.push({marketRunner:"Most Seats-Labour",current_price:2.18,prev_price:2.2,new_date:"31-08-2020",old_date:"21-08-2020"});
-data.push({marketRunner:"Most Seats-Liberal Democrat",current_price:140,prev_price:130,new_date:"31-08-2020",old_date:"21-08-2020"});
-data.push({marketRunner:"Trump To Leave Before End of 1st Term-No",current_price:1.06,prev_price:1.12,new_date:"31-08-2020",old_date:"21-08-2020"});
-data.push({marketRunner:"Trump To Leave Before End of 1st Term-Yes",current_price:15,prev_price:7.8,new_date:"31-08-2020",old_date:"21-08-2020"});
-data.push({marketRunner:"Winning Party-Any Other",current_price:1000,prev_price:610,new_date:"31-08-2020",old_date:"21-08-2020"});
-data.push({marketRunner:"Winning Party-Democrats",current_price:1.88,prev_price:1.68,new_date:"31-08-2020",old_date:"21-08-2020"});
-data.push({marketRunner:"Winning Party-Republicans",current_price:2.12,prev_price:2.44,new_date:"31-08-2020",old_date:"21-08-2020"});
+data.push({marketRunner:"Most Seats-Conservative",current_price:1.82,prev_price:1.82,new_date:"07-09-2020",old_date:"23-08-2020"});
+data.push({marketRunner:"Most Seats-Labour",current_price:2.2,prev_price:2.2,new_date:"07-09-2020",old_date:"23-08-2020"});
+data.push({marketRunner:"Most Seats-Liberal Democrat",current_price:140,prev_price:130,new_date:"07-09-2020",old_date:"23-08-2020"});
+data.push({marketRunner:"Trump To Leave Before End of 1st Term-No",current_price:1.06,prev_price:1.11,new_date:"07-09-2020",old_date:"23-08-2020"});
+data.push({marketRunner:"Trump To Leave Before End of 1st Term-Yes",current_price:12.5,prev_price:8.8,new_date:"07-09-2020",old_date:"23-08-2020"});
+data.push({marketRunner:"Winning Party-Any Other",current_price:1000,prev_price:710,new_date:"07-09-2020",old_date:"23-08-2020"});
+data.push({marketRunner:"Winning Party-Democrats",current_price:1.87,prev_price:1.74,new_date:"07-09-2020",old_date:"23-08-2020"});
+data.push({marketRunner:"Winning Party-Republicans",current_price:2.12,prev_price:2.32,new_date:"07-09-2020",old_date:"23-08-2020"});
