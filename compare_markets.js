8510d2a1a98f98e5ce3847255820916126cf8817
Jon Urwin <j.urwin@herts.ac.uk>
2020-09-15 10:10:02 +0100

    update

--- compare_markets.js
+++ compare_markets.js
@@ -1,9 +1,9 @@
 var data=[];
-data.push({marketRunner:"Most Seats-Conservative",current_price:1.82,prev_price:1.82,new_date:"14-09-2020",old_date:"13-09-2020"});
-data.push({marketRunner:"Most Seats-Liberal Democrat",current_price:140,prev_price:140,new_date:"14-09-2020",old_date:"13-09-2020"});
-data.push({marketRunner:"Most Seats-Labour",current_price:2.2,prev_price:2.22,new_date:"14-09-2020",old_date:"13-09-2020"});
-data.push({marketRunner:"Trump To Leave Before End of 1st Term-No",current_price:1.06,prev_price:1.06,new_date:"14-09-2020",old_date:"13-09-2020"});
-data.push({marketRunner:"Trump To Leave Before End of 1st Term-Yes",current_price:12,prev_price:11,new_date:"14-09-2020",old_date:"13-09-2020"});
-data.push({marketRunner:"Winning Party-Any Other",current_price:710,prev_price:690,new_date:"14-09-2020",old_date:"13-09-2020"});
-data.push({marketRunner:"Winning Party-Democrats",current_price:1.83,prev_price:1.82,new_date:"14-09-2020",old_date:"13-09-2020"});
-data.push({marketRunner:"Winning Party-Republicans",current_price:2.18,prev_price:2.2,new_date:"14-09-2020",old_date:"13-09-2020"});
+data.push({marketRunner:"Most Seats-Conservative",current_price:1.82,prev_price:1.82,new_date:"15-09-2020",old_date:"14-09-2020"});
+data.push({marketRunner:"Most Seats-Liberal Democrat",current_price:140,prev_price:140,new_date:"15-09-2020",old_date:"14-09-2020"});
+data.push({marketRunner:"Most Seats-Labour",current_price:2.2,prev_price:2.2,new_date:"15-09-2020",old_date:"14-09-2020"});
+data.push({marketRunner:"Trump To Leave Before End of 1st Term-No",current_price:1.07,prev_price:1.06,new_date:"15-09-2020",old_date:"14-09-2020"});
+data.push({marketRunner:"Trump To Leave Before End of 1st Term-Yes",current_price:11.5,prev_price:12,new_date:"15-09-2020",old_date:"14-09-2020"});
+data.push({marketRunner:"Winning Party-Any Other",current_price:810,prev_price:710,new_date:"15-09-2020",old_date:"14-09-2020"});
+data.push({marketRunner:"Winning Party-Democrats",current_price:1.85,prev_price:1.83,new_date:"15-09-2020",old_date:"14-09-2020"});
+data.push({marketRunner:"Winning Party-Republicans",current_price:2.16,prev_price:2.18,new_date:"15-09-2020",old_date:"14-09-2020"});
