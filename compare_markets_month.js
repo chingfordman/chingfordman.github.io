f26c41d4d2164ea6b9f21a37857c983ad0762e88
Jon Urwin <j.urwin@herts.ac.uk>
2020-09-27 12:07:21 +0100

    update

--- compare_markets_month.js
+++ compare_markets_month.js
@@ -1,6 +1,6 @@
 var data=[];
-data.push({marketRunner:"Trump To Leave Before End of 1st Term-Yes",current_price:9.2,prev_price:8,new_date:"25-09-2020",old_date:"18-08-2020"});
 data.push({marketRunner:"Trump To Leave Before End of 1st Term-No",current_price:1.11,prev_price:1.13,new_date:"25-09-2020",old_date:"18-08-2020"});
+data.push({marketRunner:"Trump To Leave Before End of 1st Term-Yes",current_price:9.2,prev_price:8,new_date:"25-09-2020",old_date:"18-08-2020"});
+data.push({marketRunner:"Winning Party-Democrats",current_price:1.8,prev_price:1.68,new_date:"25-09-2020",old_date:"18-08-2020"});
 data.push({marketRunner:"Winning Party-Republicans",current_price:2.22,prev_price:2.42,new_date:"25-09-2020",old_date:"18-08-2020"});
 data.push({marketRunner:"Winning Party-Any Other",current_price:830,prev_price:530,new_date:"25-09-2020",old_date:"18-08-2020"});
-data.push({marketRunner:"Winning Party-Democrats",current_price:1.8,prev_price:1.68,new_date:"25-09-2020",old_date:"18-08-2020"});
