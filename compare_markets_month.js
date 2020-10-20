722d2794076529215b22e5d29742fb771b7ad349
Jon Urwin <j.urwin@herts.ac.uk>
2020-10-20 10:10:03 +0100

    update

--- compare_markets_month.js
+++ compare_markets_month.js
@@ -1,6 +1,6 @@
 var data=[];
-data.push({marketRunner:"Trump To Leave Before End of 1st Term-No",current_price:1.08,  eventType:"Politics",prev_price:1.06,new_date:"19-10-2020",old_date:"13-09-2020"});
-data.push({marketRunner:"Trump To Leave Before End of 1st Term-Yes",current_price:11,  eventType:"Politics",prev_price:11,new_date:"19-10-2020",old_date:"13-09-2020"});
-data.push({marketRunner:"Winning Party-Any Other",current_price:1000,  eventType:"Politics",prev_price:690,new_date:"19-10-2020",old_date:"13-09-2020"});
-data.push({marketRunner:"Winning Party-Democrats",current_price:1.64,  eventType:"Politics",prev_price:1.82,new_date:"19-10-2020",old_date:"13-09-2020"});
-data.push({marketRunner:"Winning Party-Republicans",current_price:2.52,  eventType:"Politics",prev_price:2.2,new_date:"19-10-2020",old_date:"13-09-2020"});
+data.push({marketRunner:"Trump To Leave Before End of 1st Term-No",current_price:1.08,  eventType:"Politics",prev_price:1.06,new_date:"20-10-2020",old_date:"14-09-2020"});
+data.push({marketRunner:"Trump To Leave Before End of 1st Term-Yes",current_price:10.5,  eventType:"Politics",prev_price:12,new_date:"20-10-2020",old_date:"14-09-2020"});
+data.push({marketRunner:"Winning Party-Any Other",current_price:1000,  eventType:"Politics",prev_price:710,new_date:"20-10-2020",old_date:"14-09-2020"});
+data.push({marketRunner:"Winning Party-Democrats",current_price:1.59,  eventType:"Politics",prev_price:1.83,new_date:"20-10-2020",old_date:"14-09-2020"});
+data.push({marketRunner:"Winning Party-Republicans",current_price:2.68,  eventType:"Politics",prev_price:2.18,new_date:"20-10-2020",old_date:"14-09-2020"});
