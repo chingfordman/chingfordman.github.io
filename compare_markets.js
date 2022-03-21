5c18f52a5857e683ecb44e9550477d8c54ce16df
Jon Urwin <j.urwin@herts.ac.uk>
2022-03-21 09:00:06 +0000

    update

--- compare_markets.js
+++ compare_markets.js
@@ -1,6 +1,6 @@
 var data=[];
-data.push({marketRunner:"Exit Dates - Boris Johnson 2-April 2022 - June 2022",current_price:8.4,eventType:"Politics",prev_price:8.2,new_date:"21-03-2022",old_date:"18-03-2022"});
 data.push({marketRunner:"Exit Dates - Boris Johnson 2-Jan 2022 - Mar 2022",current_price:180,eventType:"Politics",prev_price:150,new_date:"21-03-2022",old_date:"18-03-2022"});
+data.push({marketRunner:"Exit Dates - Boris Johnson 2-April 2022 - June 2022",current_price:8.4,eventType:"Politics",prev_price:8.2,new_date:"21-03-2022",old_date:"18-03-2022"});
 data.push({marketRunner:"Exit Dates - Boris Johnson 2-July 2022 or later",current_price:1.12,eventType:"Politics",prev_price:1.14,new_date:"21-03-2022",old_date:"18-03-2022"});
 data.push({marketRunner:"Exit Dates - Boris Johnson-2022",current_price:3.3,eventType:"Politics",prev_price:2.9,new_date:"21-03-2022",old_date:"18-03-2022"});
 data.push({marketRunner:"Exit Dates - Boris Johnson-2023",current_price:4.1,eventType:"Politics",prev_price:4.3,new_date:"21-03-2022",old_date:"18-03-2022"});
