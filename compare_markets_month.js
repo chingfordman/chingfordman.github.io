4a837045305ce5e02e7312d4a9c5f8c05af27206
Jon Urwin <j.urwin@herts.ac.uk>
2022-04-26 09:00:08 +0100

    update

--- compare_markets_month.js
+++ compare_markets_month.js
@@ -1,9 +1,9 @@
 var data=[];
-data.push({marketRunner:"Exit Dates - Boris Johnson 2-April 2022 - June 2022",current_price:4.7,  eventType:"Politics",prev_price:8.6,new_date:"25-04-2022",old_date:"16-03-2022"});
-data.push({marketRunner:"Exit Dates - Boris Johnson 2-July 2022 or later",current_price:1.19,  eventType:"Politics",prev_price:1.12,new_date:"25-04-2022",old_date:"16-03-2022"});
-data.push({marketRunner:"Exit Dates - Boris Johnson-2022",current_price:2.08,  eventType:"Politics",prev_price:3.1,new_date:"25-04-2022",old_date:"16-03-2022"});
-data.push({marketRunner:"Exit Dates - Boris Johnson-2023",current_price:6.4,  eventType:"Politics",prev_price:4.1,new_date:"25-04-2022",old_date:"16-03-2022"});
-data.push({marketRunner:"Exit Dates - Boris Johnson-2024 or later",current_price:2.48,  eventType:"Politics",prev_price:2.08,new_date:"25-04-2022",old_date:"16-03-2022"});
-data.push({marketRunner:"Most Seats-Conservative",current_price:1.81,  eventType:"Politics",prev_price:1.66,new_date:"25-04-2022",old_date:"16-03-2022"});
-data.push({marketRunner:"Most Seats-Labour",current_price:2.2,  eventType:"Politics",prev_price:2.44,new_date:"25-04-2022",old_date:"16-03-2022"});
-data.push({marketRunner:"Most Seats-Liberal Democrat",current_price:95,  eventType:"Politics",prev_price:90,new_date:"25-04-2022",old_date:"16-03-2022"});
+data.push({marketRunner:"Exit Dates - Boris Johnson 2-April 2022 - June 2022",current_price:5.1,  eventType:"Politics",prev_price:8.8,new_date:"26-04-2022",old_date:"17-03-2022"});
+data.push({marketRunner:"Exit Dates - Boris Johnson-2022",current_price:2.12,  eventType:"Politics",prev_price:3,new_date:"26-04-2022",old_date:"17-03-2022"});
+data.push({marketRunner:"Exit Dates - Boris Johnson-2023",current_price:6.4,  eventType:"Politics",prev_price:4.2,new_date:"26-04-2022",old_date:"17-03-2022"});
+data.push({marketRunner:"Exit Dates - Boris Johnson-2024 or later",current_price:2.52,  eventType:"Politics",prev_price:2.12,new_date:"26-04-2022",old_date:"17-03-2022"});
+data.push({marketRunner:"Exit Dates - Boris Johnson 2-July 2022 or later",current_price:1.22,  eventType:"Politics",prev_price:1.12,new_date:"26-04-2022",old_date:"17-03-2022"});
+data.push({marketRunner:"Most Seats-Conservative",current_price:1.83,  eventType:"Politics",prev_price:1.66,new_date:"26-04-2022",old_date:"17-03-2022"});
+data.push({marketRunner:"Most Seats-Labour",current_price:2.18,  eventType:"Politics",prev_price:2.48,new_date:"26-04-2022",old_date:"17-03-2022"});
+data.push({marketRunner:"Most Seats-Liberal Democrat",current_price:95,  eventType:"Politics",prev_price:90,new_date:"26-04-2022",old_date:"17-03-2022"});
